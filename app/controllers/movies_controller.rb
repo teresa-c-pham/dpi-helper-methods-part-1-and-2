c886f6e61c82fc2ce14e8bac4c1ed2a2be81ace2
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-12-17 20:00:49 +0000

    Refactor links with link_to

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,6 @@
 class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
-
-    render template: "movies/new"
   end
 
   def index
@@ -16,7 +14,7 @@ class MoviesController < ApplicationController
       end
 
       format.html do
-        render({ :template => "movies/index" })
+        render "movies/index"
       end
     end
   end
@@ -28,7 +26,7 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render({ :template => "movies/show" })
+    render "movies/show"
   end
 
   def create
@@ -38,9 +36,9 @@ class MoviesController < ApplicationController
 
     if @the_movie.valid?
       @the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+      redirect_to movies_url, notice: "Movie created successfully."
     else
-      render template: "movies/new"
+      render "movies/new"
     end
   end
 
@@ -51,7 +49,7 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render({ :template => "movies/edit" })
+    render "movies/edit"
   end
 
   def update
@@ -63,9 +61,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to "/movies/#{the_movie.id}", notice: "Movie created successfully."
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => "Movie failed to update successfully." })
+      redirect_to "/movies/#{the_movie.id}", alert: "Movie failed to update successfully."
     end
   end
 
@@ -75,6 +73,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to movies_url, notice: "Movie deleted successfully."
   end
 end
